e7c66a6f07c8a2468f57de09b52ff523180387a0
khikisb <89850965+khikisb@users.noreply.github.com>
2023-05-14 21:26:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,42 +2,36 @@ import streamlit as st
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
 
+
 def encrypt_AES_IDE(plaintext, key):
     cipher = AES.new(key.encode("utf8"), AES.MODE_ECB)
     padded_plaintext = pad(plaintext.encode("utf8"), AES.block_size)
     return cipher.encrypt(padded_plaintext)
+
 
 def decrypt_AES_IDE(ciphertext, key):
     cipher = AES.new(key.encode("utf8"), AES.MODE_ECB)
     padded_plaintext = cipher.decrypt(ciphertext)
     return unpad(padded_plaintext, AES.block_size).decode("utf8")
 
+
 def app():
-    st.set_page_config(page_title="Enkripsi & Dekripsi AES-IDE", page_icon=":lock:")
-    st.title("Enkripsi & Dekripsi AES-IDE")
+    st.title("AES-IDE Encryption and Decryption")
 
-    # Input Plaintext dan Kunci
-    plaintext = st.text_input("Masukkan Plain Text : ")
-    key = st.text_input("Masukkan kunci (16 karakter) : ")
+    # Get user input
+    plaintext = st.text_input("Masukkan Plain Text")
+    key = st.text_input("Masukkan kunci harus 16 kata")
 
-    # Tombol Enkripsi
-    if st.button("Enkripsi"):
-        if len(key) == 16:
-            # Proses enkripsi
-            ciphertext = encrypt_AES_IDE(plaintext, key)
-            st.write("Ciphertext: ", ciphertext)
-        else:
-            st.error("Kunci harus memiliki 16 karakter!")
+    if plaintext and key:
+        # Proses enkripsi
+        ciphertext = encrypt_AES_IDE(plaintext, key)
+        st.write("Ciphertext: ", ciphertext)
 
-    # Tombol Dekripsi
-    if st.button("Dekripsi"):
-        if len(key) == 16:
-            # Proses dekripsi
-            decryptedtext = decrypt_AES_IDE(ciphertext, key)
-            st.write("Plaintext hasil dekripsi: ", decryptedtext)
-        else:
-            st.error("Kunci harus memiliki 16 karakter!")
+        # Proses dekripsi
+        st.write("Plaintext awal adalah : ", plaintext)
+        decryptedtext = decrypt_AES_IDE(ciphertext, key)
+        st.write("Plaintext hasil dekripsi: ", decryptedtext)
+
 
 if __name__ == "__main__":
     app()
-
